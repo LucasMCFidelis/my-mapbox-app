b04c68502b1e5f3ac2b866a1713125ddb002383f
Lucas Fidelis <lucasm241301@gmail.com>
2025-03-21 19:55:03 -0300

    Ajusta para que o mapa seja exibido em tema escuro

--- src/Map.jsx
+++ src/Map.jsx
@@ -50,6 +50,79 @@ const Map = () => {
       maxBounds: maxBounds,
     });
 
+    mapRef.current.on("style.load", () => {
+      mapRef.current.setConfigProperty("basemap", "lightPreset", "dusk");
+
+      mapRef.current.addSource("eraser", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                coordinates: [
+                  [
+                    [-0.12573, 51.53222],
+                    [-0.12458, 51.53219],
+                    [-0.12358, 51.53492],
+                    [-0.12701, 51.53391],
+                    [-0.12573, 51.53222],
+                  ],
+                ],
+                type: "Polygon",
+              },
+            },
+          ],
+        },
+      });
+
+      mapRef.current.addSource("model", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {
+            "model-uri":
+              "https://docs.mapbox.com/mapbox-gl-js/assets/tower.glb",
+          },
+          geometry: {
+            coordinates: [-0.12501974, 51.5332374],
+            type: "Point",
+          },
+        },
+      });
+
+      mapRef.current.addLayer({
+        id: "eraser",
+        type: "clip",
+        source: "eraser",
+        layout: {
+          "clip-layer-types": ["symbol", "model"],
+          "clip-layer-scope": ["basemap"],
+        },
+      });
+
+      mapRef.current.addLayer({
+        id: "tower",
+        type: "model",
+        slot: "middle",
+        source: "model",
+        minzoom: 15,
+        layout: {
+          "model-id": ["get", "model-uri"],
+        },
+        paint: {
+          "model-opacity": 1,
+          "model-rotation": [0.0, 0.0, 35.0],
+          "model-scale": [0.8, 0.8, 1.2],
+          "model-color-mix-intensity": 0,
+          "model-cast-shadows": true,
+          "model-emissive-strength": 0.8,
+        },
+      });
+    });
+
     return () => {
       if (mapRef.current) {
         mapRef.current.remove();
@@ -63,7 +136,6 @@ const Map = () => {
     if (!mapRef.current) return;
     mapRef.current.flyTo({
       center: [viewState.longitude, viewState.latitude],
-      zoom: viewState.zoom,
       essential: true,
       duration: 500,
     });
