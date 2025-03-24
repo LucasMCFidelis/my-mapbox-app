5b8dc4fe2396b44fe516b54c366844f0605ba975
Lucas Fidelis <lucasm241301@gmail.com>
2025-03-24 07:59:27 -0300

    Remove layers desnecessarias aplicadas no mapa

--- src/Map.jsx
+++ src/Map.jsx
@@ -26,6 +26,8 @@ const Map = () => {
 
   // Inicializa o mapa apenas uma vez
   useEffect(() => {
+    console.log("mapContainerRef.current:", mapContainerRef.current);
+
     if (!mapContainerRef.current || mapRef.current) return;
 
     if (!import.meta.env.VITE_MAPBOX_ACCESS_TOKEN) {
@@ -38,8 +40,8 @@ const Map = () => {
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: "mapbox://styles/mapbox/standard",
-      center: [viewState.longitude, viewState.latitude],
-      zoom: viewState.zoom,
+      center: [initialViewState.longitude, initialViewState.latitude],
+      zoom: initialViewState.zoom,
       pitch: 42,
       minZoom: 10,
       maxBounds: maxBounds,
@@ -47,75 +49,6 @@ const Map = () => {
 
     mapRef.current.on("style.load", () => {
       mapRef.current.setConfigProperty("basemap", "lightPreset", "dusk");
-
-      mapRef.current.addSource("eraser", {
-        type: "geojson",
-        data: {
-          type: "FeatureCollection",
-          features: [
-            {
-              type: "Feature",
-              properties: {},
-              geometry: {
-                coordinates: [
-                  [
-                    [-0.12573, 51.53222],
-                    [-0.12458, 51.53219],
-                    [-0.12358, 51.53492],
-                    [-0.12701, 51.53391],
-                    [-0.12573, 51.53222],
-                  ],
-                ],
-                type: "Polygon",
-              },
-            },
-          ],
-        },
-      });
-
-      mapRef.current.addSource("model", {
-        type: "geojson",
-        data: {
-          type: "Feature",
-          properties: {
-            "model-uri":
-              "https://docs.mapbox.com/mapbox-gl-js/assets/tower.glb",
-          },
-          geometry: {
-            coordinates: [-0.12501974, 51.5332374],
-            type: "Point",
-          },
-        },
-      });
-
-      mapRef.current.addLayer({
-        id: "eraser",
-        type: "clip",
-        source: "eraser",
-        layout: {
-          "clip-layer-types": ["symbol", "model"],
-          "clip-layer-scope": ["basemap"],
-        },
-      });
-
-      mapRef.current.addLayer({
-        id: "tower",
-        type: "model",
-        slot: "middle",
-        source: "model",
-        minzoom: 15,
-        layout: {
-          "model-id": ["get", "model-uri"],
-        },
-        paint: {
-          "model-opacity": 1,
-          "model-rotation": [0.0, 0.0, 35.0],
-          "model-scale": [0.8, 0.8, 1.2],
-          "model-color-mix-intensity": 0,
-          "model-cast-shadows": true,
-          "model-emissive-strength": 0.8,
-        },
-      });
     });
 
     return () => {
@@ -139,6 +72,7 @@ const Map = () => {
 
   // Atualiza os marcadores quando os eventos mudam
   useEffect(() => {
+    console.log("Eventos filtrados:", filteredEvents);
     if (!mapRef.current) return;
 
     // Remove marcadores antigos antes de adicionar novos
